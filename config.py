832edd630039fe610ae273c215487150c5e0b80a
Miqdad Yanuar Farcha <miqdad.farcha@gmail.com>
2024-10-13 13:01:03 +0700

    Add Dockerfile Support

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 # Developed by: MasterkinG32
 # Date: 2024
 # Github: https://github.com/masterking32
@@ -8,16 +9,16 @@ config = {
         # Host to listen on. For local, use "127.0.0.1".
         # For public access over the internet, use "0.0.0.0" or your public IP address.
         # Ensure the port is open in your firewall and router settings.
-        "host": "127.0.0.1",  # Default: 127.0.0.1
+        "host": os.environ['HOST'],  # Default: 127.0.0.1
         # Port to listen on. Choose a port number between 1 and 65535.
         # Ensure the chosen port is not already in use by another application.
-        "port": 3232,  # Default: 3232
+        "port": os.environ['PORT'],  # Default: 3232
     },
     "telegram_api": {
         # Telegram API credentials. Obtain your API ID and API Hash from https://my.telegram.org/apps.
-        "api_id": 1234,  # Your unique Telegram API ID
+        "api_id": os.environ['API_ID'],  # Your unique Telegram API ID
         # Your unique Telegram API Hash. Keep this value secure and do not share it publicly.
-        "api_hash": "",  # Your unique Telegram API Hash
+        "api_hash": os.environ['API_HASH'],  # Your unique Telegram API Hash
     },
     "auto_update": True,  # Automatically update the main bot. If set to True, the bot will restart after an update. Ensure you run the bot with start_linux.sh or start_windows.cmd.
     "auto_update_modules": True,  # Automatically update bot modules.
@@ -25,4 +26,3 @@ config = {
     "run_delay": 60,  # Delay in seconds before starting the bot modules. Default: 60 second.
     "display_module_logs_in_console": False,  # If set to True, module logs will be displayed in the mcf console.
     "auto_setup_accounts": False,  # Automatically set up your Telegram accounts by adding a last name and profile picture. (Adding a username is mandatory)
-}
